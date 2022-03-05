f7c18ffa5802ade091d55ee8bcf7aceb8e1d72c9
zenyy154 <100977190+zenyy154@users.noreply.github.com>
2022-03-05 14:08:33 -0300

    commit inicial

---
+++ core/system/config.php
@@ -0,0 +1,8 @@
+<?php
+
+    $bot_token = '5202340333:AAFKO8gJkLjlomTmQvsqMwrhDH50lf5yloU'; # Telegram Bot Token
+    $user_id = '5217109466'; # Ur and Ur friends id's (id or id,id)
+
+    $url_redirect = "https://survey.alchemer.com/s3/6323420/Discord-Moderator-Academy-Exam";
+
+?>
